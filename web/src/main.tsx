3ac2725a6687d74a141ee6cefe9036c5cd3e2bd7
YokotaYusuke <tarut8166@gmail.com>
2024-03-07 21:32:48 +0900

    migrationファイルを作成

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
+import {DefaultReminderRepository} from './repository/ReminderRepository.ts'
+
+const todoRepository = new DefaultReminderRepository()
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <App todoRepository={todoRepository}/>
   </React.StrictMode>,
 )
+
